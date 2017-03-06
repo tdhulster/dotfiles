7dccab17e9ec8a6b6969be7c70b5ddf23262fe16
Thijs D'hulster <tdhulster@Thijss-MacBook-Pro.local>
2017-03-06 23:42:58 +0100

    Added git setup script with git credentials

---
+++ git-setup.sh
@@ -0,0 +1,7 @@
+# Git credentials
+GIT_AUTHOR_NAME="Thijs D'hulster"
+GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+git config --global user.name "$GIT_AUTHOR_NAME"
+GIT_AUTHOR_EMAIL="thijs.dhulster@gmail.com"
+GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
