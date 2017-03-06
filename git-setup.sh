46869b59d6ad556e03e096fbc625f83323e42fa4
Thijs D\'hulster <thijs.dhulster@gmail.com>
2017-03-07 00:45:58 +0100

    fixed git setup script

--- git-setup.sh
+++ git-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Initializing Git Credentials"
+
 # Git credentials
-GIT_AUTHOR_NAME="Thijs D'hulster"
+GIT_AUTHOR_NAME="Thijs D\'hulster"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
 GIT_AUTHOR_EMAIL="thijs.dhulster@gmail.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
+
+echo "Git credentials initialized"
